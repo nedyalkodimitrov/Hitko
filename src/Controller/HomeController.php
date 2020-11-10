7758033e71e8f8d22c0dc90328b92bfe14958d9c
siskomen <az23634@gmail.com>
2020-11-10 14:56:47 +0200

    Adding a it css architecture and BEM names

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,4 +16,22 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+    /**
+     * @Route("/allCategories", name="allCategories")
+     */
+    public function allCategoriesPage()
+    {
+        return $this->render('pages/categoriesPages/allCategoriesPage.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    /**
+         * @Route("/motorcycle/a1", name="entryLevelMotorcucleCategory")
+     */
+    public function entryLevelMotorcucleCategory()
+    {
+        return $this->render('pages/categoriesPages/motorcycle/entryLevelCategory.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
 }
